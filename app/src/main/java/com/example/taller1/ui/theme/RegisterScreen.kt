96744b3600f1f0718982750142e6ac7dc7c62754
Jilmar Veloza <veloza.jilmar.2393@eam.edu.co>
2025-03-28 00:24:20 -0500

    Updated RegisterScreen.kt

--- app/src/main/java/com/example/taller1/ui/theme/RegisterScreen.kt
+++ app/src/main/java/com/example/taller1/ui/theme/RegisterScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,7 +44,8 @@ fun RegisterScreen(navController: NavController) {
         Text(
             text = "Registrarse",
             fontSize = 28.sp,
-            style = TextStyle(color = Color.Black)
+            style = TextStyle(color = Color.Black),
+            fontWeight = FontWeight.Bold
         )
 
         Spacer(modifier = Modifier.height(16.dp))
